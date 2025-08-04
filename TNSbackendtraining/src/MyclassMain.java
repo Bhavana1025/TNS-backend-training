2f2bacb801ef75a2ec8b64190f2c98655ba05e49
karusalaanil <TEJA@TEJA>
2025-08-04 17:13:32 +0530

    Initial Commit

---
+++ TNSbackendtraining/src/MyclassMain.java
@@ -0,0 +1,12 @@
+public class MyclassMain {
+	 void sayHello() {
+		 System.out.println("Hello from TNS");
+		
+	}
+	 //main function
+	 public static void main(String[]args) {
+		 MyclassMain obj = new MyclassMain();
+		 obj.sayHello();
+	 }
+
+}
