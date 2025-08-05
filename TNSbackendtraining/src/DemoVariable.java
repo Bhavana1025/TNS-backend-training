1fdbf2d31b58aea0044d8996dcb891d2a9f524e8
karusalaanil <TEJA@TEJA>
2025-08-05 20:34:41 +0530

    Variables updated

---
+++ TNSbackendtraining/src/DemoVariable.java
@@ -0,0 +1,13 @@
+
+public class DemoVariable {
+	//Instance Variable
+	int instanceVar =10;
+	//Static Variable
+	 static int staticVar =20;
+	void display() {
+		//local Variable
+		int localVar=30;
+		System.out.println(localVar);
+	}
+
+}
---
+++ TNSbackendtraining/src/DemoVariableMain.java
@@ -0,0 +1,12 @@
+
+public class DemoVariableMain {
+	public static void main(String[] args) {
+		DemoVariable obj = new DemoVariable();
+		System.out.println(obj.instanceVar);
+		System.out.println(DemoVariable.staticVar);
+		obj.display();
+
+
+	}
+
+}
