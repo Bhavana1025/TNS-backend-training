39d0c3c7b58507abf64bde6a049d45735deaf430
Bhavana1025 <bhavanavajja123@gmail.com>
2025-08-05 21:46:56 +0530

    Static Keyword

---
+++ TNSbackendtraining/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+	String name;
+	String colony;
+	String gender;
+	static int age = 20;
+	void display() {
+		System.out.println("Details of person:" +name+ " "+colony+ " "+gender+" "+age++);	
+		}
+
+}
---
+++ TNSbackendtraining/src/StudentMain.java
@@ -0,0 +1,17 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student obj = new Student();
+		obj.name ="bhavana";
+		obj.colony = "lb.nagar";
+		obj.gender = "female";
+		obj.display();
+		Student ob =new Student();
+		ob.name = "harshini";
+		ob.colony = "amberpet";
+		ob.gender = "female";
+		ob.display();
+	}
+
+}
